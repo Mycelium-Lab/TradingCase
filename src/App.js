1befa923a26e4e93cf41dd9657010c7d996b9555
Deltax2016 <deltaxussr@gmail.com>
2021-07-26 18:17:42 +0500

    Changing pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Route, IndexRoute } from 'react-router';
 import Header from './layouts/Header';
-import Staiking from './components/StaikingPage';
-import Invite from './components/InvitePage';
+import Staiking from './components/StaikingPage.jsx';
+import Invite from './components/InvitePage.jsx';
 
 import './App.css';
 import './style.css';
 import './helvetica/stylesheet.css';
 
 function App() {
+  const [page, setPage] = React.useState('staking');
+
+  function handleChange(page){
+    setPage(page);
+  }
   return (
     <div className="App">
-      <Header />
-      <Switch>
-          <Route path="/staiking" component={Staiking} exact />
-          <Route path="/invite" component={Invite} />
-          <Route path="/trade" component={Trade} />
-          <Route component={Error} />
-      </Switch>
-
+      <Header handleChange={handleChange} />
+      { page == 'staking' &&
+        <Staiking />
+      }
+      { page == 'invite' && 
+          <Invite />
+      }
       <script type="text/javascript">
         Waves.attach('.button', ['waves-button', 'waves-float']);
         Waves.init();
