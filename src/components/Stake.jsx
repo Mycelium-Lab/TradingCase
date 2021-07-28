ca2a5a894a07f805de6b0ddc889ee566aaf258ca
Deltax2016 <deltaxussr@gmail.com>
2021-07-28 18:33:40 +0500

    Fixed calculator

--- src/components/Stake.jsx
+++ src/components/Stake.jsx
@@ -1,6 +1,59 @@
 import React from 'react';
+//import calculate from 'function';
+
+const PRECISION = 10 ** 18;
+const BBDIVISOR = 10 ** 15;
+const DAILYBREW = 15 * (10**14);
+const DAILYGREW = 10 ** 12;
+const ISLOPE = 2 * (10 ** 8);
+const PEAK_PRE = 10 ** 8;
+const Minted = 0;
+
+
 
 function Stake(props) {
+
+    const [ daysAmount, setDaysAmount ] = React.useState(1000);
+    const [ stakeAmount, setStakeAmount ] = React.useState(10);
+    const [ BiggerBonus, setBiggerBonus ] = React.useState(0);
+    const [ LongerBonus, setLongerBonus ] = React.useState(0);
+    const [ EarlyBonus, setEarlyBonus ] = React.useState(0);
+    const [ InterestRate, setInterestRate ] = React.useState(0);
+    const [ apy, setApy ] = React.useState(0);
+    const [ RewardTotal, setRewardTotal ] = React.useState(0);
+
+    function toPercent(a) {
+      return (a*100/PRECISION).toFixed(4);
+    }
+
+    function calculate(amount, days) {
+
+      console.log(amount, days);
+      let BB = amount * PRECISION* PEAK_PRE / BBDIVISOR * days / 365;
+      let LB = (DAILYBREW * days) + (DAILYGREW*days*(days + 1)/2);
+      let EB = PRECISION - (ISLOPE * Minted / PEAK_PRE);
+      let IR = BB + (LB * EB) / PRECISION;
+      let RT = (amount * IR) / PRECISION;
+      setApy(36500*((RT / amount) / days))
+      setRewardTotal(RT);
+      setLongerBonus(LB);
+      setEarlyBonus(EB);
+      setBiggerBonus(BB);
+
+    }
+
+    function handleChangeStake(event) {
+      setStakeAmount(event.target.value);
+      calculate(parseInt(event.target.value), parseInt(daysAmount));
+    }
+
+    function handleChangeDays(event) {
+      setDaysAmount(event.target.value);
+      calculate(parseInt(stakeAmount), parseInt(event.target.value));
+    }
+
+    const { date } = props;
+
     return (
         <div className="tc-wrapper" id="stake-window">
         <div className="container">
@@ -11,7 +64,7 @@ function Stake(props) {
                 <div className="stake-case-input">
                   <span>Amount to stake</span>
                   <div>
-                    <input type="text" defaultValue={10} />
+                    <input type="text" value={stakeAmount} onChange={handleChangeStake} />
                     <button className="button waves-float input-button">MAX</button>
                   </div>
                   <span className="description">0.0000 CASE available - <div>Buy</div></span>
@@ -19,7 +72,7 @@ function Stake(props) {
                 <div className="stake-case-input">
                   <span>Stake time in days</span>
                   <div>
-                    <input type="text" defaultValue={1000} min={30} max={1000} />
+                    <input type="text" value={daysAmount} min={30} max={1000} onChange={handleChangeDays}/>
                     <button className="button input-button">MAX</button>
                   </div>
                   <span className="description">Min. 30 days/ Max. 1,000 days</span>
@@ -36,7 +89,7 @@ function Stake(props) {
                       <path d="M4.5 3.5H5.5V2.5H4.5V3.5ZM5 9C2.795 9 1 7.205 1 5C1 2.795 2.795 1 5 1C7.205 1 9 2.795 9 5C9 7.205 7.205 9 5 9ZM5 0C4.34339 0 3.69321 0.129329 3.08658 0.380602C2.47995 0.631876 1.92876 1.00017 1.46447 1.46447C0.526784 2.40215 0 3.67392 0 5C0 6.32608 0.526784 7.59785 1.46447 8.53553C1.92876 8.99983 2.47995 9.36812 3.08658 9.6194C3.69321 9.87067 4.34339 10 5 10C6.32608 10 7.59785 9.47322 8.53553 8.53553C9.47322 7.59785 10 6.32608 10 5C10 4.34339 9.87067 3.69321 9.6194 3.08658C9.36812 2.47995 8.99983 1.92876 8.53553 1.46447C8.07124 1.00017 7.52005 0.631876 6.91342 0.380602C6.30679 0.129329 5.65661 0 5 0ZM4.5 7.5H5.5V4.5H4.5V7.5Z" fill="#8687A7" />
                     </svg>
                   </span>
-                  <span>0.0003%</span>
+                  <span>{toPercent(BiggerBonus) + '%'}</span>
                 </div>
                 <div className="stake-case-order-details-rewards">
                   <span>Longer Bonus
@@ -44,7 +97,7 @@ function Stake(props) {
                       <path d="M4.5 3.5H5.5V2.5H4.5V3.5ZM5 9C2.795 9 1 7.205 1 5C1 2.795 2.795 1 5 1C7.205 1 9 2.795 9 5C9 7.205 7.205 9 5 9ZM5 0C4.34339 0 3.69321 0.129329 3.08658 0.380602C2.47995 0.631876 1.92876 1.00017 1.46447 1.46447C0.526784 2.40215 0 3.67392 0 5C0 6.32608 0.526784 7.59785 1.46447 8.53553C1.92876 8.99983 2.47995 9.36812 3.08658 9.6194C3.69321 9.87067 4.34339 10 5 10C6.32608 10 7.59785 9.47322 8.53553 8.53553C9.47322 7.59785 10 6.32608 10 5C10 4.34339 9.87067 3.69321 9.6194 3.08658C9.36812 2.47995 8.99983 1.92876 8.53553 1.46447C8.07124 1.00017 7.52005 0.631876 6.91342 0.380602C6.30679 0.129329 5.65661 0 5 0ZM4.5 7.5H5.5V4.5H4.5V7.5Z" fill="#8687A7" />
                     </svg>
                   </span>
-                  <span>200.0500%</span>
+                  <span>{toPercent(LongerBonus)+'%'}</span>
                 </div>
                 <div className="stake-case-order-details-rewards">
                   <span>Early Bonus
@@ -52,7 +105,7 @@ function Stake(props) {
                       <path d="M4.5 3.5H5.5V2.5H4.5V3.5ZM5 9C2.795 9 1 7.205 1 5C1 2.795 2.795 1 5 1C7.205 1 9 2.795 9 5C9 7.205 7.205 9 5 9ZM5 0C4.34339 0 3.69321 0.129329 3.08658 0.380602C2.47995 0.631876 1.92876 1.00017 1.46447 1.46447C0.526784 2.40215 0 3.67392 0 5C0 6.32608 0.526784 7.59785 1.46447 8.53553C1.92876 8.99983 2.47995 9.36812 3.08658 9.6194C3.69321 9.87067 4.34339 10 5 10C6.32608 10 7.59785 9.47322 8.53553 8.53553C9.47322 7.59785 10 6.32608 10 5C10 4.34339 9.87067 3.69321 9.6194 3.08658C9.36812 2.47995 8.99983 1.92876 8.53553 1.46447C8.07124 1.00017 7.52005 0.631876 6.91342 0.380602C6.30679 0.129329 5.65661 0 5 0ZM4.5 7.5H5.5V4.5H4.5V7.5Z" fill="#8687A7" />
                     </svg>
                   </span>
-                  <span>90.7575%</span>
+                  <span>{toPercent(EarlyBonus) + '%'}</span>
                 </div>
                 <div className="stake-case-order-details-rewards">
                   <span>APY rewards
@@ -60,7 +113,7 @@ function Stake(props) {
                       <path d="M4.5 3.5H5.5V2.5H4.5V3.5ZM5 9C2.795 9 1 7.205 1 5C1 2.795 2.795 1 5 1C7.205 1 9 2.795 9 5C9 7.205 7.205 9 5 9ZM5 0C4.34339 0 3.69321 0.129329 3.08658 0.380602C2.47995 0.631876 1.92876 1.00017 1.46447 1.46447C0.526784 2.40215 0 3.67392 0 5C0 6.32608 0.526784 7.59785 1.46447 8.53553C1.92876 8.99983 2.47995 9.36812 3.08658 9.6194C3.69321 9.87067 4.34339 10 5 10C6.32608 10 7.59785 9.47322 8.53553 8.53553C9.47322 7.59785 10 6.32608 10 5C10 4.34339 9.87067 3.69321 9.6194 3.08658C9.36812 2.47995 8.99983 1.92876 8.53553 1.46447C8.07124 1.00017 7.52005 0.631876 6.91342 0.380602C6.30679 0.129329 5.65661 0 5 0ZM4.5 7.5H5.5V4.5H4.5V7.5Z" fill="#8687A7" />
                     </svg>
                   </span>
-                  <span>66.2697%</span>
+                  <span>{apy + '%'}</span>
                 </div>
                 <div className="stake-case-order-details-rewards">
                   <span>CASE rewards
@@ -68,7 +121,7 @@ function Stake(props) {
                       <path d="M4.5 3.5H5.5V2.5H4.5V3.5ZM5 9C2.795 9 1 7.205 1 5C1 2.795 2.795 1 5 1C7.205 1 9 2.795 9 5C9 7.205 7.205 9 5 9ZM5 0C4.34339 0 3.69321 0.129329 3.08658 0.380602C2.47995 0.631876 1.92876 1.00017 1.46447 1.46447C0.526784 2.40215 0 3.67392 0 5C0 6.32608 0.526784 7.59785 1.46447 8.53553C1.92876 8.99983 2.47995 9.36812 3.08658 9.6194C3.69321 9.87067 4.34339 10 5 10C6.32608 10 7.59785 9.47322 8.53553 8.53553C9.47322 7.59785 10 6.32608 10 5C10 4.34339 9.87067 3.69321 9.6194 3.08658C9.36812 2.47995 8.99983 1.92876 8.53553 1.46447C8.07124 1.00017 7.52005 0.631876 6.91342 0.380602C6.30679 0.129329 5.65661 0 5 0ZM4.5 7.5H5.5V4.5H4.5V7.5Z" fill="#8687A7" />
                     </svg>
                   </span>
-                  <span>18.1561</span>
+                  <span>{RewardTotal}</span>
                 </div>
               </div>
             </div>
