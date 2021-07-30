5c7c0f11aeda72b24e52927a5e6494b19c5cc30b
Deltax2016 <deltaxussr@gmail.com>
2021-07-30 14:35:21 +0500

    Fixed time

--- src/layouts/TableActiveStakes.jsx
+++ src/layouts/TableActiveStakes.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+function getDays(value) {
+  var today = new Date(); // сегодня
+  var ends = new Date(today.getFullYear(), today.getMonth(), today.getDate()+value);
+  console.log(ends);
+  return Math.floor((ends - today) / (1000*60*60*24));
+}
+
+
 function TableActiveStakes(props) {
   const { activeStakes } = props;
   return (
@@ -22,11 +30,11 @@ function TableActiveStakes(props) {
           <tr>
             <td className="tg-0lax">{row.stakeAmount}</td>
             <td className="tg-0lax">{(100*parseFloat(row.apy)).toFixed(2)}</td>
-            <td className="tg-0lax">999</td>
+            <td className="tg-0lax">{getDays(parseInt(row.stakeTimeInDays))}</td>
             <td className="tg-0lax">{row.withdrawnInterestAmount}</td>
             <td className="tg-0lax">{parseFloat(row.interestAmount).toFixed(2)}</td>
             <td className="tg-0lax">{(parseFloat(row.interestAmount) + parseFloat(row.stakeAmount)).toFixed(2)}</td>
-            <td className="tg-0lax"><button>Claim Rewards</button></td>
+            <td className="tg-0lax tc-tables-referal-last-column"><button className="button referal-button">Claim Rewards</button></td>
           </tr>
         ))}
         </tbody>
--- src/timeConvert.js
+++ src/timeConvert.js
@@ -1,5 +1,3 @@
-export { setTimestamp };
-
 export default function unixToNormal(unix_timestamp) {  // переводит время из формата unix timetamp в наше родное, человеческое время. Мы же не роботы
   var date = new Date(parseInt(unix_timestamp) * 1000);
 
@@ -10,11 +8,3 @@ export default function unixToNormal(unix_timestamp) {  // переводит в
   else return day + '/' + month + '/' + year;
 }
 
-function setTimestamp(value) { // в зависимости от значения в тулбаре вычисляет timestamp 
-  var today = new Date(); // сегодня
-  if (!value) 
-    return new Date(today.getFullYear(), today.getMonth()-1, today.getDate()).getTime() / 1000; // месяц назад 
-  else if (value === 1) 
-    return new Date(today.getFullYear(), today.getMonth()-3, today.getDate()).getTime() / 1000; // 3 месяца назад
-  return new Date(today.getFullYear()-1, today.getMonth(), today.getDate()).getTime() / 1000; // год назад
-}
