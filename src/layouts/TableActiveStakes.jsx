d899d611f14562fb674b5d0da6589c7e97b9d41b
Deltax2016 <deltaxussr@gmail.com>
2021-08-04 10:49:11 +0500

    little bit redesigned active stakes

--- src/layouts/TableActiveStakes.jsx
+++ src/layouts/TableActiveStakes.jsx
@@ -14,16 +14,16 @@ function TableActiveStakes(props) {
   return (
     <div className="tc-info-block">
       <span>Active Stakes</span>
-      <table className="tg tg-scrollable-table">
+      <table className="tg tg-scrollable-table" style={{textAlign:"center"}}>
         <thead>
           <tr>
-            <th className="tg-0pky">Staked Case</th>
+            <th className="tg-0lax">Staked Case</th>
             <th className="tg-0lax">Apy</th>
             <th className="tg-0lax">Lock - Up Left</th>
             <th className="tg-0lax">Claimable Rewards</th>
             <th className="tg-0lax">Total Rewards</th>
             <th className="tg-0lax">Total Case</th>
-            <th className="tg-0lax">Actions</th>
+            <th className="tg-0lax tc-tables-referal-last-column">Actions</th>
           </tr>
         </thead>
         <tbody>
@@ -32,10 +32,10 @@ function TableActiveStakes(props) {
             <td className="tg-0lax">{row.stakeAmount}</td>
             <td className="tg-0lax">{(100*parseFloat(row.apy)).toFixed(2)}</td>
             <td className="tg-0lax">{getDays(parseInt(row.stakeTimeInDays))}</td>
-            <td className="tg-0lax">{row.withdrawnInterestAmount}</td>
+            <td className="tg-0lax" style={{width:20}}>{row.withdrawnInterestAmount}</td>
             <td className="tg-0lax">{parseFloat(row.interestAmount).toFixed(2)}</td>
             <td className="tg-0lax">{(parseFloat(row.interestAmount) + parseFloat(row.stakeAmount)).toFixed(2)}</td>
-            <td className="tg-0lax tc-tables-referal-last-column"><button className="button referal-button">Claim Rewards</button></td>
+            <td className="tg-0lax" style={{width:65}}><button className="button referal-button">Claim Rewards</button></td>
           </tr>
         ))}
         </tbody>
